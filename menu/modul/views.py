cfed581263075bc0343d8dbdbefbd6aebfb45921
fuatanshori <anshorifuat12@gmail.com>
2024-10-04 21:27:25 +0700

    bug

--- menu/modul/views.py
+++ menu/modul/views.py
@@ -124,8 +124,9 @@ def tambah_modul(request,id_levelstudy,id_mapel):
             })
         else:
             return JsonResponse({
-                "message":modul_forms.errors.as_text()
-            })
+            "message": "cant upload",
+            "errors": modul_forms.errors.get_json_data(),  # Menggunakan get_json_data untuk format yang lebih mudah dipahami di JS
+        })
     modul_forms = ModulForm(request.POST or None,request.FILES or None)
     context={
         "modul_forms": modul_forms,
