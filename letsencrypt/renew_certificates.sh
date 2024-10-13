9736ff1e91cf4b7080d5e01a3ba4a131eaa999da
fuatanshori <anshorifuat12@gmail.com>
2024-10-13 13:46:38 +0700

    docker

--- letsencrypt/renew_certificates.sh
+++ letsencrypt/renew_certificates.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-certbot renew --nginx --quiet --agree-tos --no-eff-email
+# Menunggu Nginx berjalan
+sleep 5
+
+# Membuat sertifikat SSL untuk domain dan subdomain
+certbot certonly --nginx --non-interactive --agree-tos --email your_email@example.com -d www.fuatanshori.com -d fuatanshori.com -d db.fuatanshori.com -d www.db.fuatanshori.com
+
+# Memindahkan sertifikat ke direktori host
+cp -r /etc/letsencrypt /var/www/certbot
+
+# Perintah untuk memperbarui sertifikat secara otomatis
+echo "0 0 * * * /usr/local/bin/renew_certificates.sh" | crontab -
