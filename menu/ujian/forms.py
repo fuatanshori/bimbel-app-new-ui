46f9a6e2ea39ecabf530949dcf11821a95b8815d
fuatanshori <anshorifuat12@gmail.com>
2025-01-08 21:38:35 +0700

    bug ujian edit dan package

--- menu/ujian/forms.py
+++ menu/ujian/forms.py
@@ -7,25 +7,7 @@ class SoalUjianForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['gambar_soal'].widget.attrs['class'] = 'form-control w-100'
         self.fields['pilih_jawaban_benar'].widget.attrs['class'] = 'form-select'
-
-    def clean(self):
-        cleaned_data = super().clean()
-        
-        # Field yang wajib diisi
-        required_fields = ['soal', 'jawaban_1', 'jawaban_2', 'jawaban_3', 'jawaban_4']
-        
-        # Cek setiap field required
-        for field_name in required_fields:
-            value = cleaned_data.get(field_name)
-            if not value or value.strip() == '':
-                self.add_error(field_name, f'{field_name.replace("_", " ").title()} tidak boleh kosong.')
-
-        # Validasi pilihan jawaban benar
-        pilih_jawaban = cleaned_data.get('pilih_jawaban_benar')
-        if not pilih_jawaban:
-            self.add_error('pilih_jawaban_benar', 'Pilih jawaban yang benar.')
-
-        return cleaned_data
+        self.fields["soal"].required = True
 
     class Meta:
         model = SoalUjian
