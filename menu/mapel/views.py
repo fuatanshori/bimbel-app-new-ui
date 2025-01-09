32c1a8b1eb8d5cc45be0e102f6f872dd96697474
fuatanshori <anshorifuat12@gmail.com>
2025-01-09 12:20:24 +0700

    bug kelas

--- menu/mapel/views.py
+++ menu/mapel/views.py
@@ -20,7 +20,7 @@ PAYMENT_STATUS = midtrans.PAYMENT_STATUS
 @login_required(login_url='user:masuk')
 @admin_pemateri_required
 def levelstudy_mapel(request):
-    levelstudy_objs = LevelStudy.objects.annotate(mapel_count=Count('matapelajaran')).order_by("level_study")
+    levelstudy_objs = LevelStudy.objects.annotate(mapel_count=Count('matapelajaran')).order_by("level_study","kelas")
     context = {
         'levelstudy_objs': levelstudy_objs,
     }
