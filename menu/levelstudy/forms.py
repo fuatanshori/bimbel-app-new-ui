f2a9eee279c2738d119d46e27f42687f177a73cc
fuatanshori <anshorifuat12@gmail.com>
2025-01-09 12:06:31 +0700

    bug kelas

--- menu/levelstudy/forms.py
+++ menu/levelstudy/forms.py
@@ -8,19 +8,6 @@ class LevelStudyForm(forms.ModelForm):
         self.fields['level_study'].widget.attrs['class'] = 'form-control'
         self.fields['kelas'].widget.attrs['class'] = 'form-control'
 
-    def clean(self):
-        cleaned_data = super().clean()
-        level_study = cleaned_data.get('level_study')
-        kelas_input = cleaned_data.get('kelas')
-        level_study_objs = LevelStudy.objects.filter(level_study=level_study)
-        for level_study_obj in level_study_objs:
-            kelas = level_study_obj.kelas
-            integer_kelas = [int(x) for x in kelas.split(",")]
-            if kelas_input in integer_kelas:
-                raise forms.ValidationError(f'tidak bisa ditambahkan karena {kelas_input} berada didalam rentang {integer_kelas}')
-
-        return cleaned_data
-
     class Meta:
         model = LevelStudy
         exclude=[]
